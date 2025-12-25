97442e445369314565e80b1d23d888942edd27bf
acommuni <anthony.communier@gmail.com>
2025-12-25 12:50:35 +0100

    Update finderrors.ps1
    Adapt Filter
    Add loop and keep only unique issue

--- finderrors.ps1
+++ finderrors.ps1
@@ -11,31 +11,66 @@
 # The application-specific permission settings do not grant Local Activation permission for the COM Server application with CLSID
 #$logs = Get-EventLog -LogName "System" -EntryType Error -Source "DCOM" -Newest 1 -Message "The application-specific permission settings do not grant Local Activation permission for the COM Server application with CLSID*"
 
-$EVT_MSG = "The application-specific permission settings do not grant Local Activation permission for the COM Server application with CLSID"
+$EVT_MSG = "Local Activation"
 # Search for System event log ERROR entries starting with the specified EVT_MSG
 # Level 2 is error, 3 is warning
-$logEntry = Get-WinEvent -FilterHashTable @{LogName='System'; Level=2; Id=10016} | Where-Object { $_.Message -like "$EVT_MSG*" } | Select-Object -First 1
-if (!$logEntry) {
+
+$logSeen = [System.Collections.Generic.HashSet[string]]::new()
+$checkArray = @()
+$fixArray = @()
+
+
+$logEntries = Get-WinEvent -FilterHashTable @{LogName='System'; Id=10016} | Where-Object { $_.Message -like "*$EVT_MSG*" } 
+if (!$logEntries) {
   Write-Host "No event log entries found."
   exit 1
 }
 
-Write-Host "Found an event log entry :"
-Write-Host ($logEntry | Format-List | Out-String)
-#Write-Host ($logEntry.Properties | Format-List | Out-String)
+foreach ($logEntry in $logEntries) {
 
-# Get CLSID and APPID from the event log entry
-# which we'll use to look up keys in the registry
-$CLSID = $logEntry.Properties[3].Value
-Write-Host "CLSID=$CLSID"
-$APPID = $logEntry.Properties[4].Value
-Write-Host "APPID=$APPID"
-$USERDOMAIN = $logEntry.Properties[5].Value
-Write-Host "USERDOMAIN=$USERDOMAIN"
-$USERNAME = $logEntry.Properties[6].Value
-Write-Host "USERNAME=$USERNAME"
-$USERSID = $logEntry.Properties[7].Value
-Write-Host "USERSID=$USERSID"
+ 
+  #Write-Host ($logEntry.Properties | Format-List | Out-String)
 
-Write-Host ".\checkerrors.ps1 ""$APPID"" ""$USERSID"""
-Write-Host ".\fixerrors.ps1 ""$APPID"" ""$CLSID"" ""$USERDOMAIN"" ""$USERNAME"""
+  # Get CLSID and APPID from the event log entry
+  # which we'll use to look up keys in the registry
+  $CLSID = $logEntry.Properties[3].Value
+  $APPID = $logEntry.Properties[4].Value
+  $USERDOMAIN = $logEntry.Properties[5].Value
+  $USERNAME = $logEntry.Properties[6].Value
+  $USERSID = $logEntry.Properties[7].Value
+
+  $SIG = "$CLSID-$APPID-$USERDOMAIN-$USERNAME-$USERSID"
+
+  $notSeen = $logSeen.Add($SIG)
+
+ #Write-Host "SIGNATURE=$SIG => Not seen=$notSeen"
+
+  if($notSeen){
+    Write-Host "Found an event log entry not seen before : SIGNATURE=$SIG => Not seen=$notSeen"
+    Write-Host ($logEntry | Format-List | Out-String)
+
+
+
+    Write-Host "CLSID=$CLSID"
+    Write-Host "APPID=$APPID"
+    Write-Host "USERDOMAIN=$USERDOMAIN"
+    Write-Host "USERNAME=$USERNAME"
+    Write-Host "USERSID=$USERSID"
+
+    $cmdCheck = ".\checkerrors.ps1 ""$APPID"" ""$USERSID"""
+    $checkArray += $cmdCheck
+    Write-Host $cmdCheck
+    $cmdFix = ".\fixerrors.ps1 ""$APPID"" ""$CLSID"" ""$USERDOMAIN"" ""$USERNAME"""
+    $fixArray += $cmdFix
+    Write-Host $cmdFix 
+  }
+}
+
+Write-Host "----------------"
+Write-Host "Summary"
+Write-Host "----------------"
+$logSeen | ForEach-Object { Write-Host $_ }
+Write-Host "----------------"
+$checkArray | ForEach-Object { Write-Host $_ }
+Write-Host "----------------"
+$fixArray | ForEach-Object { Write-Host $_ }
